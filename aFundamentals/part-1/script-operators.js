e493c2d46e9dccd69ca971a529a6a00e4af90ebb
Claudia Nicorici <nicoricicllaudia@yahoo.ro>
2021-03-17 15:46:19 +0000

    basic operators

---
+++ aFundamentals/part-1/script-operators.js
@@ -0,0 +1,28 @@
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+// Math operators
+const firstName = 'Claudia';
+const lastName = 'Popescu';
+console.log(firstName + " " + lastName);
+
+// Assigment operators
+let x = 10 + 5;
+x += 10; // x = x + 10;
+x *= 4;
+x++;
+x--;
+console.log(x);
+
+// Comparison operators
+console.log(ageJonas > ageSarah);
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
