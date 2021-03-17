03e3ab9b3e57e22e49faf8756fa4e03b07e6da42
Claudia Nicorici <nicoricicllaudia@yahoo.ro>
2021-03-17 13:48:11 +0000

    variables type(let, const and var)

--- aFundamentals/part-1/script.js
+++ aFundamentals/part-1/script.js
@@ -7,3 +7,40 @@ console.log(23);
 let firstName = "Jonh";
 console.log(firstName);
 console.log(firstName);
+
+//data type
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+//outpur:object   <- this is regarded as a buck or an error in JavaScript;
+console.log(typeof null);
+
+
+//let, const and var
+//use let when we need to mutate a variable or when we want to declare empty variables
+//let is block-scoped
+let age = 30;
+age = 32;
+
+//create a variable that we cannot reassign, cannot be mutated, cannot declare empty const variables;
+const birthYear = 1991;
+//birthYear = 1998;    //error
+
+//var is function-scoped
+var job = 'programmer';
+job = 'teacher';
