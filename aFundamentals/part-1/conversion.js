d117b05c11879d9b2a40c81d3568a34570c32a13
Claudia Nicorici <nicoricicllaudia@yahoo.ro>
2021-05-02 13:57:21 +0100

    truthy and falsy values

--- aFundamentals/part-1/conversion.js
+++ aFundamentals/part-1/conversion.js
@@ -18,3 +18,25 @@ console.log('23' > '18'); // output: true
 let n = '1' + 1; // -> 11
 n = n - 1;  // 11 - 1
 console.log(n); //output: 10
+
+// TRUTHY and FALSY values
+// 5 FALSY values: 0, '', undefined, null, NaN
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Jonas'));
+console.log(Boolean({}));
+console.log(Boolean(''));
+
+const money = 0;
+if (money) {
+    console.log("Don't spend it all ;)");
+} else {
+    console.log('You should get a job!');
+}
+
+let height;
+if (height) {
+    console.log('Yay! Height is defined');
+} else {
+    console.log('Height is undefined');
+}
