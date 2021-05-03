09f53e81504521d88cfe5efbb230a4d58140a3a1
Claudia Nicorici <nicoricicllaudia@yahoo.ro>
2021-05-03 18:39:53 +0100

    equality operators, == vs ===

--- aFundamentals/part-1/script-operators.js
+++ aFundamentals/part-1/script-operators.js
@@ -34,3 +34,25 @@ console.log(z, y);
 const averageAge = ageJonas + ageSarah / 2;              // output: 55.5
 const averageAgeGrouping = (ageJonas + ageSarah) / 2;    // output: 32.5
 console.log(ageJonas, ageSarah, averageAge, averageAgeGrouping);
+
+
+//equality operators: == vs ===
+// === is called the strict equality operator. It's strict,because it does not perform type coercion. Returns TRUE when both values are exactly the same type.
+// == equality operator. Does type coercion.
+const age = 18;
+if (age === 18) console.log('You just became an adult');
+if (age == 18) console.log('You just became an adult');
+
+const favourite = Number(prompt("What's your favourite number?"));
+console.log(favourite);
+console.log(typeof favourite);
+
+if (favourite === 23) {
+    console.log(`Cool! ${favourite} is an amazaing number!`)
+} else if (favourite === 7) {
+    console.log(`${favourite} is also a cool number`)
+} else {
+    console.log('Number is not 23 or 7')
+}
+
+if (favourite !== 23) console.log('Why not 23?');
